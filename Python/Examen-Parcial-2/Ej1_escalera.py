9fb0f9fb13e6432aba12dd9734c939baa39a10d6
Scar-Galilea <floresmariana475@gmail.com>
2024-12-07 09:33:37 -0600

    Nuevo directorio

---
+++ Python/Examen-Parcial-2/Ej1_escalera.py
@@ -0,0 +1,5 @@
+#Galilea Peralta Contreras.
+#07 de diciembre del 2024.
+#Descripción:
+#Este programa es una versión mejorada del juego "Piedra, papel, tijeras, lagarto, Spock".
+#Utiliza un diccionario para definir las reglas del juego y permite llevar un registro de victorias, empates y derrotas.
